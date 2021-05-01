d8b080f9a0d5b631101e42bde9d484ba215361d0
joed2355 <83504849+joed2355@users.noreply.github.com>
2021-05-01 17:24:19 -0400

    Add code to main.js

--- main.js
+++ main.js
@@ -1,1 +1,64 @@
+var numbers = process.argv.slice(2);
+var words = new Array(numbers.length);
+var output_string = "";
 
+for (var i = 0; i < words.length; i++) {
+     words[i] = "";
+}
+
+changeToWords(numbers, words);
+
+if (words[0] != "ERROR") {
+    for (var i = 0; i < words.length; i++) {
+              output_string += words[i];
+         if (i < words.length - 1) {
+              output_string += ", ";
+         }
+     }
+    console.log(output_string);
+} else {
+     console.log("ERROR: Invalid character detected. Operation aborted.");
+}
+
+function changeToWords(num_arr, word_arr) {
+    for (var i = 0; i < num_arr.length; i++) {
+        var dummy = num_arr[i].toString();
+        for (var j = 0; j < dummy.length; j++) {
+            switch (dummy.charAt(j)) {
+                case '0':
+                    word_arr[i] += ("Zero");
+                    break;
+                case '1':
+                    word_arr[i] += ("One");
+                    break;
+                case '2':
+                    word_arr[i] += ("Two");
+                    break;
+                case '3':
+                    word_arr[i] += ("Three");
+                    break;
+                case '4':
+                    word_arr[i] += ("Four");
+                    break;
+                case '5':
+                    word_arr[i] +=("Five");
+                    break;
+                case '6':
+                    word_arr[i] += ("Six");
+                    break;
+                case '7':
+                    word_arr[i] += ("Seven");
+                    break;
+                case '8':
+                    word_arr[i] += ("Eight");
+                    break;
+                case '9':
+                    word_arr[i] += ("Nine");
+                    break;
+		default:
+		    word_arr[0] = ("ERROR");
+		    return;
+            }
+        }
+    }
+}
