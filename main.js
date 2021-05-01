cab1e0f69e32a78bb0cd971594febff0bf5dcfb3
joed2355 <83504849+joed2355@users.noreply.github.com>
2021-05-01 17:28:00 -0400

    Add comments to main.js

--- main.js
+++ main.js
@@ -2,27 +2,39 @@ var numbers = process.argv.slice(2);
 var words = new Array(numbers.length);
 var output_string = "";
 
+//initialize words array
 for (var i = 0; i < words.length; i++) {
      words[i] = "";
 }
 
 changeToWords(numbers, words);
 
+//if there is no error detected, add each element into a new string to be printed without array brackets
 if (words[0] != "ERROR") {
     for (var i = 0; i < words.length; i++) {
               output_string += words[i];
+	 //if the end of the array has not been reached, add a comma and whitespace before the next element
          if (i < words.length - 1) {
               output_string += ", ";
          }
      }
+    //print the newly-formed output string
     console.log(output_string);
 } else {
+     //If the first element is equal to "ERROR", then an invalid character was detected. Print error message instead.
      console.log("ERROR: Invalid character detected. Operation aborted.");
 }
 
+//function to convert integer values into phonetic equivalent
+//parameters: 
+//	(1)num_arr: the array that holds the integer values
+//	(2)word_arr: the array that will hold the phonetic strings and an error message (should an error occur)
 function changeToWords(num_arr, word_arr) {
+    //for each element in num_arr...
     for (var i = 0; i < num_arr.length; i++) {
+	//...change it to a string...
         var dummy = num_arr[i].toString();
+	//...and store its phonetic equivalent in word_arr
         for (var j = 0; j < dummy.length; j++) {
             switch (dummy.charAt(j)) {
                 case '0':
@@ -55,6 +67,8 @@ function changeToWords(num_arr, word_arr) {
                 case '9':
                     word_arr[i] += ("Nine");
                     break;
+		//if ANY character that is not between 0 and 9 (both inclusive) is detected,
+		//make the first element of word_arr into an error message and immediately break from the function
 		default:
 		    word_arr[0] = ("ERROR");
 		    return;
